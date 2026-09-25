1e22e91909f9f3751588ff5b97b36d20c2cbfb66
Codexanuj012 <95anuj.ak@gmail.com>
2026-09-26 00:09:35 +0530

    After the all complete and Deploy Phase

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: "https://agentiq-1.onrender.com",
+    credentials: true
 }))
 
 app.use(express.json())
